f8244a71db814456f00643478fa51071e50ed56f
Eisuke Watanabe <eisuke88@icloud.com>
2021-12-26 16:37:31 +0900

    fix-headerContentLayout

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -3,12 +3,25 @@
     <div class="c-HeaderContent__history">
       <v-fa :icon="['far', 'clock']" />
     </div>
-    <form action="">
-      <button class="c-HeaderContent__input">
-        <v-fa icon="search" />
-        <span class="c-HeaderContent__inputPlaceholder">Search SmartRound</span>
-      </button>
-    </form>
+    <div class="c-HeaderContent__searchContainer">
+      <form action="">
+        <button class="c-HeaderContent__input">
+          <v-fa icon="search" />
+          <span class="c-HeaderContent__inputPlaceholder"
+            >Search SmartRound</span
+          >
+        </button>
+      </form>
+    </div>
+    <div class="c-HeaderContent__help">
+      <v-fa :icon="['far', 'question-circle']" />
+      <img
+        src="../assets/logo.png"
+        class="c-base_icon c-base_icon--image c-HeaderContent__img"
+        role="img"
+        alt=""
+      />
+    </div>
   </div>
 </template>
 
@@ -29,11 +42,20 @@ export default Vue.extend({
     margin-right: 24px;
     color: rgb(156, 156, 156);
     font-size: 19px;
+    text-align: right;
+    flex: 1;
+    min-width: 260px;
+  }
+
+  &__searchContainer {
+    min-width: 0;
+    max-width: 732px;
+    flex: 2;
   }
 
   &__input {
-    width: 560px;
-    flex: 5;
+    min-width: 732px;
+    flex: 2;
     height: 24px;
     border-radius: 6px;
     background-color: rgba(156, 156, 156, 0.2);
@@ -44,6 +66,28 @@ export default Vue.extend({
 
   &__inputPlaceholder {
     margin-left: 8px;
+  }
+
+  &__help {
+    font-size: 19px;
+    color: rgb(156, 156, 156);
+    flex: 1;
+    margin-left: auto;
+    min-width: 260px;
+    padding-left: 32px;
+    margin-right: 16px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  &__img {
+    height: 28px;
+    width: 28px;
+    margin-left: 16px;
+    margin-right: 16px;
+    border-radius: 4px;
+    overflow: hidden;
   }
 }
 </style>
